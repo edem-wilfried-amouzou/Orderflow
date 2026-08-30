ea89567480373af83934260d2c46c04df9e15e35
edem-wilfried-amouzou <edemwilfriedamouzou@gmail.com>
2026-08-30 15:02:23 +0000

    rconstruction du backend pour le front

--- commandes/dashboard_urls.py
+++
@@ -1,9 +0,0 @@
-# commandes/dashboard_urls.py
-from django.urls import path
-from .views import DashboardStatsView, DashboardActivite7jView, DashboardFinancesView
-
-urlpatterns = [
-    path('stats/', DashboardStatsView.as_view(), name='dashboard-stats'),
-    path('activite-7j/', DashboardActivite7jView.as_view(), name='dashboard-activite'),
-    path('finances/', DashboardFinancesView.as_view(), name='dashboard-finances'),
-]
