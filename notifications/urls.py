e83eb563ad05194aeeae1ae2d3180053e2eb95b2
edem-wilfried-amouzou <edemwilfriedamouzou@gmail.com>
2026-09-04 19:36:19 +0000

    chore: nettoyage de la racine pour la structure worktrees

--- notifications/urls.py
+++
@@ -1,9 +0,0 @@
-# notifications/urls.py
-from django.urls import path
-from .views import NotificationListView, NotificationMarquerLuView, PushTokenCreateView
-
-urlpatterns = [
-    path('', NotificationListView.as_view(), name='notification-list'),
-    path('<int:pk>/lu/', NotificationMarquerLuView.as_view(), name='notification-lu'),
-    path('push-token/', PushTokenCreateView.as_view(), name='push-token'),
-]
